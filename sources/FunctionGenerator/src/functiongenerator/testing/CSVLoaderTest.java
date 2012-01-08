143ae651764dd35f04a88d7f66f94b8b1c92abaf
Piotr Jessa <dzesse@gmail.com>
2012-01-08 01:07:06 +0100

    Added failing tests for CSVLoader

--- sources/FunctionGenerator/src/functiongenerator/testing/CSVLoaderTest.java
+++ sources/FunctionGenerator/src/functiongenerator/testing/CSVLoaderTest.java
@@ -1,19 +1,54 @@
 package functiongenerator.testing;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import functiongenerator.loaders.CSVLoader;
+import functiongenerator.ui.PointsTableModel;
 
 public class CSVLoaderTest {
-	
-	@Test
-	public void test_loading_empty_file(){
-		Assert.fail("Should fail");
+
+	private CSVLoader loader;
+	private File file;
+	private PointsTableModel result;
+
+	@Before
+	public void set_up() {
+		loader = new CSVLoader();
 	}
-	
+
+	@Test(expected = FileNotFoundException.class)
+	public void test_loading_non_exitsting_file() throws IOException {
+		file = new File("some_none_existing_file");
+		loader.loadFromFile(file, Integer.class);
+	}
+
 	@Test
-	public void test_loading_sample_0_file(){
+	public void test_loading_empty_file_should_pass() throws IOException {
+		file = new File("empty.csv");
+		result = loader.loadFromFile(file, Integer.class);
 		
+		Assert.assertEquals(0, result.getRowCount());
 	}
-	
+
+	@Test
+	public void test_loading_sample_0_file_integers() throws IOException {
+		file = new File("sample_0.csv");
+		result = loader.loadFromFile(file, Integer.class);
+		
+		Assert.assertEquals(4, result.getRowCount());
+	}
+
+	@Test
+	public void test_loading_sample_0_file_doubles() throws IOException {
+		file = new File("sample_0.csv");
+		result = loader.loadFromFile(file, Double.class);
+		
+		Assert.assertEquals(4, result.getRowCount());
+	}
 }
